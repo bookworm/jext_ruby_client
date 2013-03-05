539de689aafa31165b4118fcacb1e5e3c5f8acbc
Ken Erickson <bookworm.productions@gmail.com>
2013-03-04 22:20:07 -0800

    add some methods to Extension resource

--- lib/jext/resources/Extension.rb
+++ lib/jext/resources/Extension.rb
@@ -3,7 +3,33 @@ module Jext
     include Model
     include Her::Model
     uses_api Jext.api
+
+    ## Associations
     has_one    :extension
     belongs_to :extension
+
+    ##
+    # Methods
+    #
+
+    def support_update_engine?(engines=[])
+      return (engines & supported_update_engines).empty?
+    end
+
+    def parent?
+      return self.parent
+    end
+
+    def child?
+      return self.child
+    end
+
+    def secure?
+      return self.secure
+    end
+
+    def support_updates?
+      return self.support_updates
+    end
   end
 end
